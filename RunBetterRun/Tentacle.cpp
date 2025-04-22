e2f2c00f1e04d61938455383eb87ba386fddd1c3
stupidiot <shng6815@gmail.com>
2025-04-23 07:17:22 +0900

    주석 제거 및 각도 계산 개선
    `Tentacle::Action` 함수에서 플레이어와 텐타클 간의 상대 위치 벡터 계산 관련 주석을 제거했습니다.
    `angleDiff` 계산에서 나누기 2를 제거하여 각도 차이를 직접적으로 계산하도록 변경했습니다.
    최종 각도 계산에서 주석을 제거하고, 각도 정규화 부분은 유지했습니다.
    이러한 변경은 코드의 가독성을 높이고, 각도 차이를 보다 직관적으로 계산하도록 개선되었습니다.

--- RunBetterRun/Tentacle.cpp
+++ RunBetterRun/Tentacle.cpp
@@ -16,41 +16,31 @@ void Tentacle::Action(void)
 
 		float moveAngle = RAD_TO_DEG(atan2(dy,dx));
 
-		// 각도 정규화 (0-360도 범위로)
 		if(moveAngle < 0)
 			moveAngle += 360.0f;
 
-		// 플레이어 카메라 정보 가져오기
 		auto player = Player::GetInstance();
 		FPOINT playerPos = player->GetCameraPos();
 		FPOINT cameraDir = player->GetCameraVerDir();
 
-		// 플레이어-텐타클 상대 위치 벡터 계산
 		float pdx = playerPos.x - sprite.pos.x;
-		float pdy = -(playerPos.y - sprite.pos.y); // Y축 뒤집기
+		float pdy = -(playerPos.y - sprite.pos.y);
 
-		// 플레이어 시점에서 본 텐타클의 각도 계산
 		float viewAngle = RAD_TO_DEG(atan2(pdy,pdx));
 		if(viewAngle < 0) viewAngle += 360.0f;
 
-		// 플레이어 카메라 방향 각도 계산
 		float cameraAngle = RAD_TO_DEG(atan2(-cameraDir.y,cameraDir.x));
 		if(cameraAngle < 0) cameraAngle += 360.0f;
 
-		// 텐타클의 이동 방향과 플레이어 시점의 각도 차이 계산
-		float angleDiff = (moveAngle - viewAngle)/2;
+		float angleDiff = (moveAngle - viewAngle);
 
-		// 최종 프레임 각도 계산 (카메라 각도 고려)
 		float finalAngle = (cameraAngle - viewAngle + angleDiff + 180.0f);
 
-		// 각도 정규화
 		while(finalAngle >= 360.0f) finalAngle -= 360.0f;
 		if(finalAngle < 0) finalAngle += 360.0f;
 
-		// 프레임 인덱스 계산 (10도당 1프레임)
 		int frameY = static_cast<int>(finalAngle / 10.0f) % 36;
 
-		// 애니메이션 프레임 업데이트
 		sprite.aniInfo.currentFrame.y = frameY;
 	}
 }
