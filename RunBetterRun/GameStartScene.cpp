6b45d2b0b1eeb1128abe9f13e9ecaecad7e50a33
Baekbanjang <baekgang624@naver.com>
2025-04-22 14:28:32 +0900

    GameStartScene 수정
    리소스 및 디자인 수정

--- RunBetterRun/GameStartScene.cpp
+++ RunBetterRun/GameStartScene.cpp
@@ -7,14 +7,14 @@
 HRESULT GameStartScene::Init()
 {
 	image = ImageManager::GetInstance()->
-		AddImage("게임시작",L"Image/start_scene.bmp",1536,1024,1,2);
+		AddImage("게임시작",L"Image/eleveator_scene.bmp",1920,1080,1,2);
 
 	if(image == nullptr)
 	{
 		return E_FAIL;
 	}
 
-	titleText = L"Horror Game";
+	titleText = L"Escape from the \napartment";
 	InitButtons();
 	MapManager::GetInstance()->Init(L"Map/SavedMap.dat");
 	//MapManager::GetInstance()->Init(L"Map/EditorMap.dat");
@@ -78,11 +78,11 @@ void GameStartScene::InitButtons()
 
 	RECT rc;
 	GetClientRect(g_hWnd,&rc);
-	int centerX = rc.right / 2;
+	int centerX = rc.right / 1.35;
 	int startY = rc.bottom / 2;
-	int buttonWidth = 200;
-	int buttonHeight = 50;
-	int buttonSpacing = 20;
+	int buttonWidth = 250;
+	int buttonHeight = 40;
+	int buttonSpacing = 50;
 
 	// 버튼 생성
 	buttons.resize(3);
@@ -173,19 +173,21 @@ void GameStartScene::DrawTitle(HDC hdc)
 
 	HFONT hFont = CreateFont(60,0,0,0,FW_NORMAL,FALSE,FALSE,FALSE,
 							 DEFAULT_CHARSET,OUT_DEFAULT_PRECIS,CLIP_DEFAULT_PRECIS,
-							 DEFAULT_QUALITY,DEFAULT_PITCH | FF_DONTCARE,TEXT("BleedingPixels"));
+							 DEFAULT_QUALITY,DEFAULT_PITCH | FF_DONTCARE,TEXT("Chainsaw Carnage"));
 
 	HFONT oldFont = (HFONT)SelectObject(hdc,hFont);
 
-	// 제목 위치 계산 (화면 상단에서 1/3 위치)
+	int titleWidth = 800;
+	int titleHeight = 160;
+
 	RECT titleRect = {
-		0,
-		rc.bottom / 3 - 60,
-		rc.right,
-		rc.bottom / 3
+		rc.right - titleWidth,
+		rc.bottom / 4,  
+		rc.right - 20,
+		rc.bottom / 3 + titleHeight
 	};
 
-	DrawText(hdc,titleText,-1,&titleRect,DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+	DrawText(hdc,titleText,-1,&titleRect,DT_CENTER | DT_VCENTER);
 
 	SelectObject(hdc,oldFont);
 	DeleteObject(hFont);
@@ -194,41 +196,47 @@ void GameStartScene::DrawTitle(HDC hdc)
 void GameStartScene::DrawButton(HDC hdc,Button & button)
 {
 
-	COLORREF bgColor;
-	switch(button.state) {
-	case ButtonState::NORMAL:
-		bgColor = RGB(50,50,50); 
-		break;
+	COLORREF textColor = RGB(220, 220, 220);
 
-	case ButtonState::HOVER:
-		bgColor = RGB(80,80,80);  
-		break;
-
-	case ButtonState::CLICKED:
-		bgColor = RGB(120,120,120);  
-		break;
+	if(button.state == ButtonState::HOVER)
+	{
+		textColor = RGB(255, 0,0 );
 	}
-
 	// 버튼 그리기
-	HBRUSH hBrush = CreateSolidBrush(bgColor);
-	HBRUSH oldBrush = (HBRUSH)SelectObject(hdc,hBrush);
 
-	RoundRect(hdc,button.rect.left,button.rect.top,button.rect.right,button.rect.bottom,20,20);
 
-	SetTextColor(hdc,RGB(255,255,255));  
+	SetTextColor(hdc, textColor);  
 	SetBkMode(hdc,TRANSPARENT);          
 
 	// 버튼 텍스트용 폰트
-	HFONT hFont = CreateFont(24,0,0,0,FW_NORMAL,FALSE,FALSE,FALSE,
+	HFONT hFont = CreateFont(40,0,0,0,FW_NORMAL,FALSE,FALSE,FALSE,
 							 DEFAULT_CHARSET,OUT_DEFAULT_PRECIS,CLIP_DEFAULT_PRECIS,
-							 DEFAULT_QUALITY,DEFAULT_PITCH | FF_DONTCARE,TEXT("BleedingPixels"));
+							 DEFAULT_QUALITY,DEFAULT_PITCH | FF_DONTCARE,TEXT("Chainsaw Carnage"));
 	HFONT oldFont = (HFONT)SelectObject(hdc,hFont);
 
 
 	DrawText(hdc,button.text,-1,&button.rect,DT_CENTER | DT_VCENTER | DT_SINGLELINE);
 
+	// 호버 상태일 때 밑줄 그리기
+	if(button.state == ButtonState::HOVER || button.state == ButtonState::CLICKED) {
+		SIZE textSize;
+		GetTextExtentPoint32(hdc,button.text,wcslen(button.text),&textSize);
+
+		int lineX1 = button.rect.left + (button.rect.right - button.rect.left - textSize.cx) / 2;
+		int lineX2 = lineX1 + textSize.cx;
+		int lineY = button.rect.bottom - 5;  // 텍스트 아래 위치
+
+		HPEN underlinePen = CreatePen(PS_SOLID,2,textColor);
+		HPEN oldPen = (HPEN)SelectObject(hdc,underlinePen);
+
+		MoveToEx(hdc,lineX1,lineY,NULL);
+		LineTo(hdc,lineX2,lineY);
+
+		SelectObject(hdc,oldPen);
+		DeleteObject(underlinePen);
+	}
+
 	SelectObject(hdc,oldFont);
 	DeleteObject(hFont);
-	SelectObject(hdc,oldBrush);
-	DeleteObject(hBrush);
+
 }
